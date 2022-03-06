46bc4c60238ca1f06a313e87a3bcf021ad4f40e2
ayodeawe <jawe@nvidia.com>
2022-03-06 11:37:22 -0600

    syntax fix

--- vars/getTestStages.groovy
+++ vars/getTestStages.groovy
@@ -1,14 +1,3 @@
-def test_configs  = [
-  [label: "driver-450", cuda_ver: "11.0", py_ver: "3.8", os: "centos7", arc: "ARM"],
-  [label: "driver-495", cuda_ver: "11.2", py_ver: "3.9", os: "ubuntu18.04", arc: "ARM"],
-  [label: "driver-495", cuda_ver: "11.5", py_ver: "3.9", os: "ubuntu20.04", arc: "ARM"],
-  
-  // repeat the same combinations for AMD arc
-  [label: "driver-450", cuda_ver: "11.0", py_ver: "3.8", os: "centos7", arc: "AMD"],
-  [label: "driver-495", cuda_ver: "11.2", py_ver: "3.9", os: "ubuntu18.04", arc: "AMD"],
-  [label: "driver-495", cuda_ver: "11.5", py_ver: "3.9", os: "ubuntu20.04", arc: "AMD"],
-]
-
 def generateStage(test_config) {
   return {
     stage("Test - ${test_config.label} - ${test_config.cuda_ver} - ${test_config.py_ver} - ${test_config.os}") {
@@ -23,6 +12,16 @@ def generateStage(test_config) {
 }
 
 def call() {
+  def test_configs  = [
+    [label: "driver-450", cuda_ver: "11.0", py_ver: "3.8", os: "centos7", arc: "ARM"],
+    [label: "driver-495", cuda_ver: "11.2", py_ver: "3.9", os: "ubuntu18.04", arc: "ARM"],
+    [label: "driver-495", cuda_ver: "11.5", py_ver: "3.9", os: "ubuntu20.04", arc: "ARM"],
+    
+    // repeat the same combinations for AMD arc
+    [label: "driver-450", cuda_ver: "11.0", py_ver: "3.8", os: "centos7", arc: "AMD"],
+    [label: "driver-495", cuda_ver: "11.2", py_ver: "3.9", os: "ubuntu18.04", arc: "AMD"],
+    [label: "driver-495", cuda_ver: "11.5", py_ver: "3.9", os: "ubuntu20.04", arc: "AMD"],
+  ]
   node {
     return test_configs.collectEntries {
       ["${it.arc}: ${it.label} - ${it.cuda_ver} - ${it.py_ver} - ${it.os}" : generateStage(it)]
