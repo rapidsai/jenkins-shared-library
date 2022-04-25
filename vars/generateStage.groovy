3d10a003396382d1805173dd272ac5cd2503727d
AJ Schmidt <aschmidt@nvidia.com>
2022-04-25 10:25:40 -0400

    rename `arc` to `arch`

--- vars/generateStage.groovy
+++ vars/generateStage.groovy
@@ -7,7 +7,7 @@ def generateTestStage(test_config, steps) {
           .inside("""
             --runtime=nvidia
             -e NVIDIA_VISIBLE_DEVICES=$EXECUTOR_NUMBER
-            -e ARC=${test_config.arc}
+            -e ARCH=${test_config.arch}
             -e CUDA=${test_config.cuda_ver}
             -e PY_VER=${test_config.py_ver}
             -e HOME=$WORKSPACE
@@ -28,7 +28,7 @@ def generateNightlyTestStage(test_config, steps) {
             .inside("""
               --runtime=nvidia
               -e NVIDIA_VISIBLE_DEVICES=$EXECUTOR_NUMBER
-              -e ARC=${test_config.arc}
+              -e ARCH=${test_config.arch}
               -e CUDA=${test_config.cuda_ver}
               -e PY_VER=${test_config.py_ver}
               -e HOME=$WORKSPACE
@@ -47,7 +47,7 @@ def generateCudaBuildStage(test_config, steps) {
         docker
           .image(getStageImg(test_config, true))
           .inside("""
-            -e ARC=${test_config.arc}
+            -e ARCH=${test_config.arch}
           """) {
           steps()
         }
@@ -63,7 +63,7 @@ def generatePythonBuildStage(test_config, steps) {
         docker
           .image(getStageImg(test_config, true))
           .inside("""
-            -e ARC=${test_config.arc}
+            -e ARCH=${test_config.arch}
             -e PY_VER=${test_config.py_ver}
           """) {
           steps()
@@ -76,31 +76,31 @@ def generatePythonBuildStage(test_config, steps) {
 def call(stage, Closure steps) {
   parallels_config = [
     branch_pr_test: [
-      // [label: "driver-495-arm", cuda_ver: "11.2", py_ver: "3.9", os: "ubuntu18.04", arc: "arm64"],
-      // [label: "driver-495-arm", cuda_ver: "11.5", py_ver: "3.9", os: "ubuntu20.04", arc: "arm64"],
+      // [label: "driver-495-arm", cuda_ver: "11.2", py_ver: "3.9", os: "ubuntu18.04", arch: "arm64"],
+      // [label: "driver-495-arm", cuda_ver: "11.5", py_ver: "3.9", os: "ubuntu20.04", arch: "arm64"],
 
-      // [label: "driver-450", cuda_ver: "11.0", py_ver: "3.8", os: "centos7", arc: "amd64"],
-      // [label: "driver-495", cuda_ver: "11.2", py_ver: "3.9", os: "ubuntu18.04", arc: "amd64"],
-      [label: "driver-495", cuda_ver: "11.5", py_ver: "3.9", os: "ubuntu20.04", arc: "amd64"],
+      // [label: "driver-450", cuda_ver: "11.0", py_ver: "3.8", os: "centos7", arch: "amd64"],
+      // [label: "driver-495", cuda_ver: "11.2", py_ver: "3.9", os: "ubuntu18.04", arch: "amd64"],
+      [label: "driver-495", cuda_ver: "11.5", py_ver: "3.9", os: "ubuntu20.04", arch: "amd64"],
     ],
     nightly_test: [
-      [label: "driver-495-arm", cuda_ver: "11.2", py_ver: "3.9", os: "ubuntu18.04", arc: "arm64"],
-      [label: "driver-495-arm", cuda_ver: "11.5", py_ver: "3.9", os: "ubuntu20.04", arc: "arm64"],
+      [label: "driver-495-arm", cuda_ver: "11.2", py_ver: "3.9", os: "ubuntu18.04", arch: "arm64"],
+      [label: "driver-495-arm", cuda_ver: "11.5", py_ver: "3.9", os: "ubuntu20.04", arch: "arm64"],
 
-      [label: "driver-450", cuda_ver: "11.0", py_ver: "3.8", os: "centos7", arc: "amd64"],
-      [label: "driver-495", cuda_ver: "11.2", py_ver: "3.9", os: "ubuntu18.04", arc: "amd64"],
-      [label: "driver-495", cuda_ver: "11.5", py_ver: "3.9", os: "ubuntu20.04", arc: "amd64"],
+      [label: "driver-450", cuda_ver: "11.0", py_ver: "3.8", os: "centos7", arch: "amd64"],
+      [label: "driver-495", cuda_ver: "11.2", py_ver: "3.9", os: "ubuntu18.04", arch: "amd64"],
+      [label: "driver-495", cuda_ver: "11.5", py_ver: "3.9", os: "ubuntu20.04", arch: "amd64"],
 
     ],
     cuda_build: [
-        [arc: "arm64", label: "cpu4-arm64", os: "centos7", cuda_ver: "11.5"],
-        [arc: "amd64", label: "cpu4-amd64", os: "centos7", cuda_ver: "11.5"]
+        [arch: "arm64", label: "cpu4-arm64", os: "centos7", cuda_ver: "11.5"],
+        [arch: "amd64", label: "cpu4-amd64", os: "centos7", cuda_ver: "11.5"]
     ],
     python_build: [
-        [arc: "arm64", py_ver: "3.8", label: "cpu-arm64", cuda_ver: "11.5", os: "ubuntu18.04"],
-        [arc: "arm64", py_ver: "3.9", label: "cpu-arm64", cuda_ver: "11.5", os: "ubuntu18.04"],
-        [arc: "amd64", py_ver: "3.8", label: "cpu", cuda_ver: "11.5", os: "centos7"],
-        [arc: "amd64", py_ver: "3.9", label: "cpu", cuda_ver: "11.5", os: "centos7"],
+        [arch: "arm64", py_ver: "3.8", label: "cpu-arm64", cuda_ver: "11.5", os: "ubuntu18.04"],
+        [arch: "arm64", py_ver: "3.9", label: "cpu-arm64", cuda_ver: "11.5", os: "ubuntu18.04"],
+        [arch: "amd64", py_ver: "3.8", label: "cpu", cuda_ver: "11.5", os: "centos7"],
+        [arch: "amd64", py_ver: "3.9", label: "cpu", cuda_ver: "11.5", os: "centos7"],
     ]
   ]
   return generateStage(stage, parallels_config, steps)
@@ -110,19 +110,19 @@ def generateStage(stage, parallels_config, steps) {
   switch(stage) {
     case "branch_pr_test":
       return parallels_config[stage].collectEntries {
-        ["${it.arc}: ${it.label} - ${it.cuda_ver} - ${it.py_ver} - ${it.os}" : generateTestStage(it, steps)]
+        ["${it.arch}: ${it.label} - ${it.cuda_ver} - ${it.py_ver} - ${it.os}" : generateTestStage(it, steps)]
       }
     case "nightly_test":
       return parallels_config[stage].collectEntries {
-        ["${it.arc}: ${it.label} - ${it.cuda_ver} - ${it.py_ver} - ${it.os}" : generateNightlyTestStage(it, steps)]
+        ["${it.arch}: ${it.label} - ${it.cuda_ver} - ${it.py_ver} - ${it.os}" : generateNightlyTestStage(it, steps)]
       }
     case "cuda_build":
       return parallels_config[stage].collectEntries {
-        ["${it.arc}: ${it.label}" : generateCudaBuildStage(it, steps)]
+        ["${it.arch}: ${it.label}" : generateCudaBuildStage(it, steps)]
       }
     case "python_build":
       return parallels_config[stage].collectEntries {
-        ["${it.arc}: ${it.label} -${it.py_ver}" : generatePythonBuildStage(it, steps)]
+        ["${it.arch}: ${it.label} -${it.py_ver}" : generatePythonBuildStage(it, steps)]
       }
     default: throw new Exception("Invalid stage name provided")
   }
@@ -138,7 +138,7 @@ def getStageImg(config, is_build_stage) {
     img += "-driver"
   }
 
-  if (config.arc === "arm64") {
+  if (config.arch === "arm64") {
     img += "-arm64"
   }
 
